423ecaddd4d30489cac88fbf442ed27aaad9f3d8
migsamu <mufrikiler@gmail.com>
2021-10-28 10:20:25 +0200

    comunicacion entre pantallas

---
+++ app/src/main/java/org/iesfm/proyectactivities/MainActivity.java
@@ -0,0 +1,21 @@
+package org.iesfm.proyectactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cambiarActividad(View v) {
+        Intent pantallaSecundaria = new Intent(this, MainActivity2.class);
+        startActivity(pantallaSecundaria);
+    }
+}
