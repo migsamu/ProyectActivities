1c7277f862c708b6aab9885310da4f4d4019efb6
migsamu <mufrikiler@gmail.com>
2021-10-29 12:40:01 +0200

    añadidos compartir datos

--- app/src/main/java/org/iesfm/proyectactivities/MainActivity.java
+++ app/src/main/java/org/iesfm/proyectactivities/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static final String PROYECT_ACTIVITIES_NAME = "org.iesfm.proyectactivities.name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void cambiarActividad(View v) {
         Intent pantallaSecundaria = new Intent(this, MainActivity2.class);
+        EditText etName = (EditText) findViewById(R.id.etName);
+        String name = etName.getText().toString();
+        pantallaSecundaria.putExtra(PROYECT_ACTIVITIES_NAME, name);
+
         startActivity(pantallaSecundaria);
     }
 }
--- app/src/main/java/org/iesfm/proyectactivities/MainActivity2.java
+++ app/src/main/java/org/iesfm/proyectactivities/MainActivity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,6 +13,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent pantallaPrincipal = getIntent();
+        String name = pantallaPrincipal.getStringExtra(MainActivity.PROYECT_ACTIVITIES_NAME);
+        TextView tvName = (TextView) findViewById(R.id.tvName);
+        tvName.setText("Hola " + name);
     }
 
     public void cambiarActividad(View v) {
